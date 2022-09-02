adcb94cf4d9fe43fd1d2dd0dee9c90169161388d
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-09-02 23:58:56 +0700

    same defaults

--- src/main/java/io/otel/pyroscope/PyroscopeAutoConfigurationCustomizerProvider.java
+++ src/main/java/io/otel/pyroscope/PyroscopeAutoConfigurationCustomizerProvider.java
@@ -3,23 +3,31 @@ package io.otel.pyroscope;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
 
 @AutoService(AutoConfigurationCustomizerProvider.class)
 public class PyroscopeAutoConfigurationCustomizerProvider
         implements AutoConfigurationCustomizerProvider {
     @Override
     public void customize(AutoConfigurationCustomizer autoConfiguration) {
-        autoConfiguration.addTracerProviderCustomizer((tpBuilder, cfg) -> tpBuilder.addSpanProcessor(
-                new PyroscopeSpanProcessor(
-                        new PyroscopeConfiguration.Builder()
-                                .setAppName(cfg.getString("otel.pyroscope.app.name"))
-                                .setPyroscopeEndpoint(cfg.getString("otel.pyroscope.endpoint"))
-                                .setRootSpanOnly(cfg.getBoolean("otel.pyroscope.root.span.only", true))
-                                .setAddSpanName(cfg.getBoolean("otel.pyroscope.add.span.name", true))
-                                .setAddProfileURL(cfg.getBoolean("otel.pyroscope.add.profile.url", true))
-                                .setAddProfileBaselineURLs(cfg.getBoolean("otel.pyroscope.add.profile.baseline.url", true))
-                                .build()
-                )));
+        autoConfiguration.addTracerProviderCustomizer((tpBuilder, cfg) -> {
+            boolean startProfiling = cfg.getBoolean("otel.pyroscope.start.profiling", false);
+            if (startProfiling) {
+                PyroscopeAgent.start(Config.build());
+            }
+            return tpBuilder.addSpanProcessor(
+                    new PyroscopeSpanProcessor(
+                            new PyroscopeConfiguration.Builder()
+                                    .setAppName(cfg.getString("otel.pyroscope.app.name"))
+                                    .setPyroscopeEndpoint(cfg.getString("otel.pyroscope.endpoint"))
+                                    .setRootSpanOnly(cfg.getBoolean("otel.pyroscope.root.span.only", true))
+                                    .setAddSpanName(cfg.getBoolean("otel.pyroscope.add.span.name", false))
+                                    .setAddProfileURL(cfg.getBoolean("otel.pyroscope.add.profile.url", false))
+                                    .setAddProfileBaselineURLs(cfg.getBoolean("otel.pyroscope.add.profile.baseline.url", false))
+                                    .build()
+                    ));
+        });
     }
 
 }
